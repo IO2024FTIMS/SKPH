ff9e29a4c0f5e7542b046f9c59f5457544f8950d
wst3pniewski <wstepniewski.dev@zohomail.eu>
2024-10-27 14:36:12 +0100

    project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,36 @@
 from flask import Flask
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import text
+
 
 from config import Config
+from app.extensions import db
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
     # Initialize Flask extensions here
-
+    db.init_app(app)
+    
     # Register blueprints here
     
-    @app.route('/test/')
-    def test_page():
-        return '<h1>Test route!</h1>'
+    
+    @app.route('/')
+    def hello():
+        return "Hello World!!"
+    
+    @app.route('/db')
+    def check_db():
+        try:
+            with db.engine.connect() as connection:
+                result = connection.execute(text("SELECT CURRENT_TIMESTAMP")).scalar()
+                print(result)
+                if result:
+                    return f"Database connection is established. Current timestamp: {result}"
+                else:
+                    return "Database connection failed."
+        except SQLAlchemyError as e:
+            return f"Database connection failed: {str(e)}"
 
     return app
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'secret'
+    # os.environ.get('SECRET_KEY')
+    
+    # postgresql+psycopg2://DATABASE_USER:PASSWORD@DATABASE_HOST_NAME:DATABASE_PORT/DATABASE_NAME
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
