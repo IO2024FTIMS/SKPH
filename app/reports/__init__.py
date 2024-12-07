e795b3fa9b25e9d4dae950ff2b00b011ca5e69d1
Mati <m.luzak@outlook.com>
2024-12-07 14:14:14 +0100

    moduł/tworznie podstawowej strukóry modułu

--- app/reports/__init__.py
+++ app/reports/__init__.py
@@ -1,1 +1,13 @@
 __author__ = "Maciej Kowalski, Mateusz Luzak"
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    # narazie bez inicjalizacji bazy danych
+
+    #from app.reports.routes import reports_bp
+    #app.register_blueprint(reports_bp, url_prefix='/reports')
+
+    return app
