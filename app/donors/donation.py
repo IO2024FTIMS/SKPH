40e19f3e79f91dcb6141028040243476b402258e
Wojciech Sierocki <zapachyzpodpachy@gmail.com>
2025-01-15 12:53:54 +0100

    Add files via upload
    dodane ronzen Qdodanitonvojsbfijhbverbv UUUUAAAAZAAAAA PIWOKO

--- app/donors/donation.py
+++ app/donors/donation.py
@@ -2,14 +2,13 @@ from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from donor import Base
 from sqlalchemy.orm import relationship
 
-class Donation(Base):
+class DonatioMoney(Base):
     __tablename__ = "donations"  # Nazwa tabeli w bazie danych
 
     donation_id = Column(Integer, primary_key=True, index=True)
     description = Column(String, nullable=False)
     donation_date = Column(Date, nullable=False)
-    donation_type = Column(String, nullable=False)
-    amount = Column(Float, nullable=False)
+    cashAmount = Column(Float, nullable=False)
     donor_id = Column(Integer, ForeignKey("donors.donor_id"))
 
     # Relacja z tabelą Donor
@@ -17,7 +16,26 @@ class Donation(Base):
 
     def return_confirmation(self) -> str:
         """Return a confirmation message."""
-        return f"Donation confirmed: {self.description}, Amount: {self.amount}"
+        return f"Donation confirmed: {self.description}, Amount: {self.cashAmount}"
 
     def __repr__(self):
-        return f"<Donation(description={self.description}, amount={self.amount})>"
+        return f"<Donation(description={self.description}, amount={self.cashAmount})>"
+
+class DonationItem(Base):
+    __tablename__ = "donations"  # Nazwa tabeli w bazie danych
+
+    donation_id = Column(Integer, primary_key=True, index=True)
+    description = Column(String, nullable=False)
+    donation_date = Column(Date, nullable=False)
+    number = Column(Float, nullable=False)
+    donor_id = Column(Integer, ForeignKey("donors.donor_id"))
+
+    # Relacja z tabelą Donor
+    donor = relationship("Donor", back_populates="donations")
+
+    def return_confirmation(self) -> str:
+        """Return a confirmation message."""
+        return f"Donation confirmed: {self.description}, Amount: {self.number}"
+
+    def __repr__(self):
+        return f"<Donation(description={self.description}, amount={self.number})>"
