68410fa54a1e7e81800e1e0fafb00be0dac98cab
247751mo <247751@edu.p.lodz.pl>
2025-01-17 18:14:23 +0100

    affected i requests

--- app/models/affected.py
+++ app/models/affected.py
@@ -11,8 +11,10 @@ class Affected(db.Model):
     needs = db.Column(db.Text)
     address = relationship('Address')
     address_id = mapped_column(ForeignKey('address.id'))
-
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # Add the missing relationship
+    requests = relationship('Request', back_populates='affected')
 
     def __repr__(self):
         return f'Affected(id={self.id}, needs={self.needs})'
---
+++ app/models/request.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from xmlrpc.client import DateTime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.types import Enum as SQLEnum
+from app.extensions import db
+
+class RequestStatus(Enum):
+    PENDING = "Pending"
+    APPROVED = "Approved"
+    REJECTED = "Rejected"
+    COMPLETED = "Completed"
+
+class Request(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    status: Mapped[RequestStatus] = mapped_column(SQLEnum(RequestStatus), nullable=False)
+    address = relationship('Address')
+    address_id = mapped_column(ForeignKey('address.id'))
+    affected_id = mapped_column(ForeignKey('affected.id'))
+    needs: Mapped[str]
+
+    affected = relationship('Affected', back_populates='requests')
+
+    def __repr__(self):
+        return (
+            f'Request:(id={self.id!r}, name={self.name!r}, '
+            f'status={self.status.value!r},  affected_id={self.affected_id!r})'
+        )
