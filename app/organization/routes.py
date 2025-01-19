12e20ddada564197970a226cada86b6476ef2c87
wst3pniewski <wstepniewski.dev@zohomail.eu>
2025-01-19 14:16:16 +0100

    organizations template and logic

--- app/organization/routes.py
+++ app/organization/routes.py
@@ -6,8 +6,14 @@ from app.models.address import Address
 from app.models.authorities import Authorities
 from app.models.charity_campaign import CharityCampaign, OrganizationCharityCampaign
 from app.models.organization import Organization
+from app.models.volunteer import Volunteer
 
 bp = Blueprint('organization', __name__, template_folder='../templates/organization')
+
+
+@bp.route('/')
+def index():
+    return render_template('organization_index.jinja')
 
 
 @bp.route('/charity_campaigns')
@@ -39,6 +45,38 @@ def add_sample_organization_charity_campaign():
     sample_campaign = CharityCampaign(name="Sample Campaign", description="This is a sample charity campaign.", authority=authority)
     o1 = Organization(organization_name='Organization', description='desc', approved=True, address=a1)
     sample_organization_campaign = OrganizationCharityCampaign(organization=o1, charity_campaign=sample_campaign)
+
+    v1 = Volunteer(first_name='John', last_name='Black', email='jblack@mail.com', phone='111111111')
+    aa1 = Address(street='Miejska', street_number='1a', city='Łódź', voivodeship='Łódzkie')
+    v1.address = aa1
+    sample_organization_campaign.volunteers.append(v1)
+
+    db.session.add(v1)
     db.session.add(sample_organization_campaign)
     db.session.commit()
     return redirect(url_for('organization.list_organization_charity_campaigns'))
+
+
+@bp.route('/authorities/<int:authorities_id>')
+def view_authorities(authorities_id):
+    authority = db.session.get(Authorities, authorities_id)
+    return render_template('view_authority.jinja', authorities=authority)
+
+
+@bp.route('organization/<int:organization_id>')
+def view_organization(organization_id):
+    o1 = db.session.get(Organization, organization_id)
+    return render_template('view_organization.jinja', organization=o1)
+
+
+@bp.route('charity_campaign/<int:charity_campaign_id>')
+def view_campaign(charity_campaign_id):
+    cp = db.session.get(CharityCampaign, charity_campaign_id)
+    return render_template('view_charity_campaign.jinja', campaign=cp)
+
+
+@bp.route('/charity_campaign/<int:charity_campaign_id>/volunteers')
+def list_volunteers(charity_campaign_id):
+    campaign = db.session.get(OrganizationCharityCampaign, charity_campaign_id)
+    volunteers = campaign.volunteers  # if campaign and campaign.authority else []
+    return render_template('list_volunteers.jinja', volunteers=volunteers)
