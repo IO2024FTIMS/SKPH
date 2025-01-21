1c3f068903804e9873e36517f4c9d00bfada1751
inspite-radio <242336@edu.p.lodz.pl>
2025-01-21 21:43:01 +0100

    inlined test

--- app/maps/routes.py
+++ app/maps/routes.py
@@ -6,6 +6,8 @@ from app import db
 from app.models.map import POI, DangerArea, ReliefArea, Coordinates
 
 
+TEST_MODE: bool = False
+
 ORS_API_KEY = "Dodam_potem"
 ors_client = openrouteservice.Client(key=ORS_API_KEY)
 
@@ -20,7 +22,52 @@ bp = Blueprint(
 
 @bp.route("/")
 def index():
-    # TEST()
+    if TEST_MODE:
+        # Dodawanie punktów POI
+        coord1 = Coordinates(x=51.74708, y=19.45404)
+        coord2 = Coordinates(x=51.74800, y=19.45500)
+        db.session.add(coord1)
+        db.session.add(coord2)
+
+        poi1 = POI(name="Point A", coordinates=coord1, status=True)
+        poi2 = POI(name="Point B", coordinates=coord2, status=True)
+        db.session.add(poi1)
+        db.session.add(poi2)
+
+        # Dodanie pięciokątnej strefy zagrożenia
+        pentagon_coords = [
+            [51.7475, 19.4530],
+            [51.7480, 19.4535],
+            [51.7485, 19.4545],
+            [51.7475, 19.4550],
+            [51.7470, 19.4540],
+        ]
+        danger_area = DangerArea(
+            name="Pentagon Danger Zone",
+            coordinates=pentagon_coords,
+            status=True,
+        )
+        db.session.add(danger_area)
+
+        # Dodanie dziewięciokątnej strefy pomocy
+        nonagon_coords = [
+            [51.7465, 19.4525],
+            [51.7470, 19.4530],
+            [51.7475, 19.4535],
+            [51.7480, 19.4540],
+            [51.7485, 19.4545],
+            [51.7480, 19.4550],
+            [51.7475, 19.4555],
+            [51.7470, 19.4550],
+            [51.7465, 19.4545],
+        ]
+        relief_area = ReliefArea(
+            name="Nonagon Relief Zone", coordinates=nonagon_coords, status=True
+        )
+        db.session.add(relief_area)
+
+        db.session.commit()
+    # endof test
 
     # Filtrowanie po statusie jako boolean
     pois = POI.query.filter_by(status=True).all()
@@ -53,51 +100,6 @@ def get_route(start_id, end_id):
         return {"route": route_geometry}
     else:
         return {"error": "Unable to calculate route"}, 500
-
-
-def TEST():
-    # Dodawanie punktów POI
-    coord1 = Coordinates(x=51.74708, y=19.45404)
-    coord2 = Coordinates(x=51.74800, y=19.45500)
-    db.session.add(coord1)
-    db.session.add(coord2)
-
-    poi1 = POI(name="Point A", coordinates=coord1, status=True)
-    poi2 = POI(name="Point B", coordinates=coord2, status=True)
-    db.session.add(poi1)
-    db.session.add(poi2)
-
-    # Dodanie pięciokątnej strefy zagrożenia
-    pentagon_coords = [
-        [51.7475, 19.4530],
-        [51.7480, 19.4535],
-        [51.7485, 19.4545],
-        [51.7475, 19.4550],
-        [51.7470, 19.4540],
-    ]
-    danger_area = DangerArea(
-        name="Pentagon Danger Zone", coordinates=pentagon_coords, status=True
-    )
-    db.session.add(danger_area)
-
-    # Dodanie dziewięciokątnej strefy pomocy
-    nonagon_coords = [
-        [51.7465, 19.4525],
-        [51.7470, 19.4530],
-        [51.7475, 19.4535],
-        [51.7480, 19.4540],
-        [51.7485, 19.4545],
-        [51.7480, 19.4550],
-        [51.7475, 19.4555],
-        [51.7470, 19.4550],
-        [51.7465, 19.4545],
-    ]
-    relief_area = ReliefArea(
-        name="Nonagon Relief Zone", coordinates=nonagon_coords, status=True
-    )
-    db.session.add(relief_area)
-
-    db.session.commit()
 
 
 def calculate_route(start, end):
