42f874c30c58818a7152c72677d2741960469e02
AbsZeroO <138863520+AbsZeroO@users.noreply.github.com>
2025-01-23 14:07:26 +0100

    Zmiana routes

--- app/maps/routes.py
+++ app/maps/routes.py
@@ -1,6 +1,5 @@
 import openrouteservice
 from flask import Blueprint, render_template, request, jsonify
-from sqlalchemy import false
 from app.extensions import db
 from app.models.map import POI, DangerArea, ReliefArea, Coordinates
 
@@ -12,14 +11,9 @@ bp = Blueprint('maps', __name__,
                static_folder='static',
                static_url_path='maps')
 
-flaga = False
 
 @bp.route('/')
 def index():
-    global flaga
-    if flaga:
-        TEST()
-        flaga = False
 
     pois = POI.query.filter_by(status=True).all()
     danger_areas = DangerArea.query.filter_by(status=True).all()
@@ -134,49 +128,6 @@ def delete_relief_area(area_id):
         return jsonify({"success": True, "message": "Relief Area deleted successfully"})
     else:
         return jsonify({"success": False, "message": "Relief Area not found"}), 404
-
-
-
-def TEST():
-    coord1 = Coordinates(x=51.74708, y=19.45404)
-    coord2 = Coordinates(x=51.74800, y=19.45500)
-    db.session.add(coord1)
-    db.session.add(coord2)
-
-    poi1 = POI(name="Point A", coordinates=coord1, status=True, type="punktDystrybucji")
-    poi2 = POI(name="Point B", coordinates=coord2, status=True, type="poszkodowani")
-    db.session.add(poi1)
-    db.session.add(poi2)
-
-    pentagon_coords = [
-        [51.7475, 19.4530],
-        [51.7480, 19.4535],
-        [51.7485, 19.4545],
-        [51.7475, 19.4550],
-        [51.7470, 19.4540]
-    ]
-    danger_area = DangerArea(name="Pentagon Danger Zone",
-                             coordinates=pentagon_coords,
-                             status=True)
-    db.session.add(danger_area)
-
-    nonagon_coords = [
-        [51.7465, 19.4525],
-        [51.7470, 19.4530],
-        [51.7475, 19.4535],
-        [51.7480, 19.4540],
-        [51.7485, 19.4545],
-        [51.7480, 19.4550],
-        [51.7475, 19.4555],
-        [51.7470, 19.4550],
-        [51.7465, 19.4545]
-    ]
-    relief_area = ReliefArea(name="Nonagon Relief Zone",
-                             coordinates=nonagon_coords,
-                             status=True)
-    db.session.add(relief_area)
-
-    db.session.commit()
 
 
 def calculate_route(start, end):
