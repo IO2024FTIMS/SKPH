5af0fba2cc07bdb9d2353cddfa35ec19dc569ba5
wst3pniewski <wstepniewski.dev@zohomail.eu>
2025-01-23 15:59:43 +0100

    fix

--- app/communication/routes.py
+++ app/communication/routes.py
@@ -13,64 +13,66 @@ bp = Blueprint('chat', __name__,
                static_url_path='communication')
 
 
-def init_routes(bp):
-    @bp.route('/')
-    def index():
-        users = User.query.all()
-        return render_template('communication/index.html', users=users)
+@bp.route('/')
+def index():
+    users = User.query.all()
+    return render_template('communication/index.html', users=users)
 
-    @bp.route('/chat')
-    def chat():
-        if not current_user.is_authenticated:
-            return redirect(url_for('auth.login'))
-        user = User.query.filter_by(id=current_user.id).first()
-        if not user:
-            return redirect(url_for('index'))
 
-        # Znajdź wszystkich użytkowników, z którymi dany użytkownik prowadził rozmowy
-        chat_users = db.session.query(User).join(
-            Message,
-            or_(Message.sender_id == User.id, Message.receiver_id == User.id)
-        ).filter(
-            or_(Message.sender_id == user.id, Message.receiver_id == user.id)
-        ).distinct().all()
+@bp.route('/chat')
+def chat():
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+    user = User.query.filter_by(id=current_user.id).first()
+    if not user:
+        return redirect(url_for('index'))
 
-        chat_users = [u for u in chat_users if u.email != current_user.email]
+    # Znajdź wszystkich użytkowników, z którymi dany użytkownik prowadził rozmowy
+    chat_users = db.session.query(User).join(
+        Message,
+        or_(Message.sender_id == User.id, Message.receiver_id == User.id)
+    ).filter(
+        or_(Message.sender_id == user.id, Message.receiver_id == user.id)
+    ).distinct().all()
 
-        return render_template('communication/chat.html', user=user, chat_users=chat_users)
+    chat_users = [u for u in chat_users if u.email != current_user.email]
 
-    @bp.route('/search_users')
-    def search_users():
-        current_email = request.args.get('current_email')
-        query = request.args.get('query', '')
+    return render_template('communication/chat.html', user=user, chat_users=chat_users)
 
-        users = User.query.filter(
-            User.email.ilike(f'%{query}%'),
-            User.email != current_email
-        ).all()
 
-        return jsonify([{'email': user.email} for user in users])
+@bp.route('/search_users')
+def search_users():
+    current_email = request.args.get('current_email')
+    query = request.args.get('query', '')
 
-    @bp.route('/get_messages')
-    def get_messages():
-        sender_email = request.args.get('sender')
-        receiver_email = request.args.get('receiver')
+    users = User.query.filter(
+        User.email.ilike(f'%{query}%'),
+        User.email != current_email
+    ).all()
 
-        sender = User.query.filter_by(email=sender_email).first()
-        receiver = User.query.filter_by(email=receiver_email).first()
+    return jsonify([{'email': user.email} for user in users])
 
-        if not sender or not receiver:
-            return jsonify([])
 
-        messages = Message.query.filter(
-            or_(
-                (Message.sender_id == sender.id) & (Message.receiver_id == receiver.id),
-                (Message.sender_id == receiver.id) & (Message.receiver_id == sender.id)
-            )
-        ).order_by(Message.timestamp).all()
+@bp.route('/get_messages')
+def get_messages():
+    sender_email = request.args.get('sender')
+    receiver_email = request.args.get('receiver')
 
-        return jsonify([{
-            'sender': message.sender.email,
-            'content': message.content,
-            'timestamp': message.timestamp.strftime('%Y-%m-%d %H:%M:%S')
-        } for message in messages])
+    sender = User.query.filter_by(email=sender_email).first()
+    receiver = User.query.filter_by(email=receiver_email).first()
+
+    if not sender or not receiver:
+        return jsonify([])
+
+    messages = Message.query.filter(
+        or_(
+            (Message.sender_id == sender.id) & (Message.receiver_id == receiver.id),
+            (Message.sender_id == receiver.id) & (Message.receiver_id == sender.id)
+        )
+    ).order_by(Message.timestamp).all()
+
+    return jsonify([{
+        'sender': message.sender.email,
+        'content': message.content,
+        'timestamp': message.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    } for message in messages])
