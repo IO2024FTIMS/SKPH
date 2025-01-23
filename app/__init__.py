26b89ca602ff9f4dd8ec8010770eb2994672334d
starko0 <148477786+starko0@users.noreply.github.com>
2025-01-23 16:04:13 +0100

    Feature/chat (#22)
    Co-authored-by: Wiktor <74744189+wsmlw@users.noreply.github.com>
    Co-authored-by: wst3pniewski <wstepniewski.dev@zohomail.eu>
    Co-authored-by: tomasz <tomasz@test.com>

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
-from app.maps.routes import bp as maps_bp
 from app.affected.routes import bp as affected_bp
 from app.auth.routes import bp as auth_bp
 from app.auth.user_service import init_login_manager
+from app.communication.routes import bp as chat_bp
+from app.communication.socketio_chat import socketio
 from app.donors.routes import bp as donors_bp
 from app.extensions import babel, db, get_locale, mail
+from app.maps.routes import bp as maps_bp
 from app.organization.routes import bp as organization_bp
 from app.reports.routes import bp as reports_bp
 from app.volunteers.routes import bp as volunteers_bp
@@ -23,6 +25,7 @@ def create_app(config_class=Config):
     babel.init_app(flask_app, locale_selector=get_locale)
     init_login_manager(flask_app)
     mail.init_app(flask_app)
+    socketio.init_app(flask_app)
 
     with flask_app.app_context():
         db.create_all()
@@ -33,6 +36,8 @@ def create_app(config_class=Config):
     flask_app.register_blueprint(reports_bp, url_prefix='/reports')
 
     flask_app.register_blueprint(volunteers_bp, url_prefix='/volunteers')
+
+    flask_app.register_blueprint(chat_bp, url_prefix="/communication")
 
     flask_app.register_blueprint(affected_bp, url_prefix='/affected')
 
