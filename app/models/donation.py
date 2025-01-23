f139ab68e14c6bf3528320e289d186cb4fedc1a0
wst3pniewski <161605621+wst3pniewski@users.noreply.github.com>
2025-01-23 23:20:01 +0100

    Feature/donations (#36)

--- app/models/donation.py
+++ app/models/donation.py
@@ -1,11 +1,21 @@
-from typing import TYPE_CHECKING
 from datetime import date
+from enum import Enum
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from app.extensions import db
 
 if TYPE_CHECKING:
     from app.models.donor import Donor
+
+
+class ItemDonationType(Enum):
+    WATER = "water"
+    FOOD = "food"
+    CLOTHES = "clothes"
+    MEDICINE = "medicine"
 
 
 class DonationMoney(db.Model):
@@ -31,8 +41,8 @@ class DonationItem(db.Model):
     donationItem_id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str]
     donation_date: Mapped[date] = mapped_column(default=date.today)
-    donation_type: Mapped[str]
-    number: Mapped[float]
+    donation_type: Mapped[ItemDonationType]
+    number: Mapped[int]
     charity_campaign = relationship('OrganizationCharityCampaign', back_populates='donations_item')
     charity_campaign_id = mapped_column(ForeignKey('organization_charity_campaign.id'))
     donor_id: Mapped[int] = mapped_column(ForeignKey('donor.donor_id'))
