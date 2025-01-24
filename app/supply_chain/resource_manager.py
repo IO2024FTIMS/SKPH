f67dfbf17c10f3090c6c52d3ca1990619adc4cfe
Piotr Polakowski <p.polakowski@proton.me>
2025-01-24 22:42:21 +0100

    Merged with fix/feature/supply-chain

--- app/supply_chain/resource_manager.py
+++ app/supply_chain/resource_manager.py
@@ -1,28 +1,14 @@
 from app.extensions import db
 
-from datetime import datetime
-
 from app.models.assigned_resources import AssignedResourcesStock
 from app.models.item_stock import ItemStock
-from app.models.volunteer import Volunteer
-from app.models.organization import Organization
-from app.models.address import Address
-from app.models.affected import Affected
 from app.models.donation import DonationItem
-from app.models.donation import DonationMoney
-from app.models.donor import Donor
-from app.models.user import User
 from app.models.charity_campaign import CharityCampaign
-from app.models.charity_campaign import OrganizationCharityCampaign
 
 
 class ResourceManager:
-    def init(self):
-        self.test = 'test'
-
     # select * from donation_item d
-    # join charity_campagin c on d.charity_campagin_id=c.id 
-
+    # join charity_campagin c on d.charity_campagin_id=c.id
     def get_all_donations_for_campagin(self, charity_campaign):
         item_donations = db.session.query(DonationItem).join(CharityCampaign) \
             .filter(DonationItem.charity_campaign_id == charity_campaign.id) \
@@ -37,7 +23,7 @@ class ResourceManager:
 
     def assign_resources_to_affected(self, dict_affected, resource_used_amount, resource):
         if resource_used_amount <= resource.amount:
-            for key, value in dict_affected.items():
+            for key in dict_affected.items():
                 assigned = AssignedResourcesStock(item_type_id=resource.item_type_id, campaign_id=resource.campaign_id,
                                                   amount=resource_used_amount, affected_id=key)
                 db.session.add(assigned)
--- app/supply_chain/routes.py
+++ app/supply_chain/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, redirect, render_template, request
 
 from sqlalchemy import text
 
@@ -13,43 +13,28 @@ from app.models.charity_campaign import OrganizationCharityCampaign
 from app.models.address import Address
 from app.models.affected import Affected
 from app.models.donation import DonationItem, DonationType
-from app.models.donation import DonationMoney
 from app.models.user import User
 from app.models.donor import Donor
 from app.models.authorities import Authorities
 from app.models.request import Request
-
 from app.models.charity_campaign import CharityCampaign
-from app.models.charity_campaign import OrganizationCharityCampaign
-
 from app.supply_chain.resource_manager import ResourceManager
 
-bp = Blueprint('supply_chain', __name__, 
+bp = Blueprint('supply_chain', __name__,
                template_folder='../templates/supply_chain',
                static_folder='static',
                static_url_path='supply-chain')
 
-# @bp.route('/')
-# def index():
-#     return render_template('supply_chain.jinja',
-#                             item_donations=None,
-#                             charity_campaign=None,
-#                             volunteers=None) 
-
-
-
 resource_manager = ResourceManager()
-    
+
 
 @bp.route('/')
 def index():
-    volunteers = db.session.query(Volunteer)
     charity_campaign = db.session.query(CharityCampaign).all()
-
     if len(charity_campaign) == 0:
         return render_template('supply_chain.jinja',
-                               charity_campaign = None,
-                               item_donations = None)
+                               charity_campaign=None,
+                               item_donations=None)
     query = text('''select ist.id, dt.type, ist.amount  from item_stock ist
         join donation_type dt on ist.item_type_id = dt.id''')
 
@@ -60,12 +45,12 @@ def index():
         .join(DonationType, Request.donation_type_id == DonationType.id) \
         .join(Affected, Request.affected_id == Affected.id) \
         .join(Address, Request.req_address_id == Address.id) \
-        .add_columns(Request.name, Request.status, Affected.first_name, Affected.last_name, Address.city, 
-                    Address.street, Address.voivodeship, DonationType.type, Request.amount) \
+        .add_columns(Request.name, Request.status, Affected.first_name, Affected.last_name,
+                     Address.city, Address.street, Address.voivodeship, DonationType.type, Request.amount) \
         .all()
     print(current_requests)
 
-    return render_template('supply_chain.jinja', 
+    return render_template('supply_chain.jinja',
                            item_donations=item_donations,
                            charity_campaign=charity_campaign,
                            volunteers=avalible_volunteers,
@@ -80,39 +65,35 @@ def truncate():
     return redirect('/supply-chain')
 
 
-
-@bp.route('/resource/<int:id>', methods=['GET', 'POST'])
-def manage_resource(id):
-    resource = db.session.get(ItemStock, id)
-    if request.method =='GET':
+@bp.route('/resource/<int:item_id>', methods=['GET', 'POST'])
+def manage_resource(item_id):
+    resource = db.session.get(ItemStock, item_id)
+    if request.method == 'GET':
         print('resource: ')
         print(resource)
         resource_type = db.session.get(DonationType, resource.item_type_id)
         return render_template('manage_resource.jinja',
                                resource=resource,
                                resource_type=resource_type,
-                               affecteds = db.session.query(Affected).all())
-    if request.method =='POST':
+                               affecteds=db.session.query(Affected).all())
+    if request.method == 'POST':
         affected_assigned_resources = {}
-        resouce_use_amount =0
+        resouce_use_amount = 0
         for i in db.session.query(Affected).all():
             if int(request.form[i.id]) > 0:
                 affected_assigned_resources[i.id] = int(request.form[i.id])
-                resouce_use_amount+=int(request.form[i.id])
+                resouce_use_amount += int(request.form[i.id])
         print(resource)
-        #resource_manager.assign_resources_to_affected(affected_assigned_resources,resouce_use_amount, resource)
         return redirect('/')
-
 
 
 @bp.route('/add-data')
 def add_data():
-    
     address1 = Address(
-    street="123 Main St",
-    street_number="Apt 101",
-    city="New York",
-    voivodeship="NY"
+        street="123 Main St",
+        street_number="Apt 101",
+        city="New York",
+        voivodeship="NY"
     )
 
     address2 = Address(
@@ -134,18 +115,13 @@ def add_data():
         db.session.rollback()
         charity_campaign = db.session.query(CharityCampaign).all()
         if len(charity_campaign) == 0:
-            return render_template('supply_chain.jinja', 
-                            item_donations=None,
-                            charity_campaign=None)
-        else: 
+            return render_template('supply_chain.jinja', item_donations=None, charity_campaign=None)
+        else:
             charity_campaign = charity_campaign[0]
-            query = text('select ist.id, dt.type, ist.amount  from item_stock ist join donation_type dt on ist.item_type_id = dt.id')
+            query = text('select ist.id, dt.type, ist.amount from item_stock ist join donation_type dt on ist.item_type_id = dt.id')
             item_donations = db.session.execute(query).mappings().all()
-            return render_template('supply_chain.jinja', 
-                            item_donations=item_donations,
-                            charity_campaign=charity_campaign)
-    else: 
-
+            return render_template('supply_chain.jinja', item_donations=item_donations, charity_campaign=charity_campaign)
+    else:
         db.session.add(address1)
         db.session.add(address2)
         db.session.add(address3)
@@ -171,20 +147,19 @@ def add_data():
             type="authorities"
         )
 
-        user4= User(
+        user4 = User(
             email="affected@email.com",
             password_hash='pass222',
             active=True,
             type="affected"
         )
 
-        user5= User(
+        user5 = User(
             email="org@email.com",
             password_hash='pass222',
             active=True,
             type="organization"
         )
-
 
         db.session.add(user1)
         db.session.add(user2)
@@ -200,7 +175,6 @@ def add_data():
             phone_number="123-456-7890",
             email="john.doe2@example.com",
             user=user1
-            #user_id=User.query.filter(User.email == "john.doe@example.com").first().id
         )
 
         donor2 = Donor(
@@ -217,11 +191,8 @@ def add_data():
             user_id=User.query.filter(User.email == "jane.smith234@example.com").first().id
         )
         db.session.add(authority)
-
-
         db.session.add(donor1)
         db.session.add(donor2)
-
         charity_campaign_1 = CharityCampaign(
             name="Charity campagin 1",
             description="Sample campagin",
@@ -229,19 +200,16 @@ def add_data():
         )
 
         db.session.add(charity_campaign_1)
-    
-
         organization1 = Organization(
             organization_name="FUNDACJA OJCA RYDZYKA",
             description="bardzo dobra organizacja",
             approved=True,
             address_id=Address.query.filter(Address.street == "456 Elm St" and Address.street_number == 'Apt 202' and Address.city == 'Los Angeles').first().id,
-            user_id = User.query.filter(User.email == "org@email.com").first().id,
+            user_id=User.query.filter(User.email == "org@email.com").first().id,
         )
 
         db.session.add(organization1)
-
-        organization_charity_campaign_1 =  OrganizationCharityCampaign(
+        organization_charity_campaign_1 = OrganizationCharityCampaign(
             charity_campaign=charity_campaign_1,
             organization=organization1
         )
@@ -252,19 +220,18 @@ def add_data():
         donation_type_3 = DonationType(type='Clothes')
         donation_type_4 = DonationType(type='Other')
 
-
         db.session.add(donation_type_2)
         db.session.add(donation_type_3)
         db.session.add(donation_type_4)
 
         # donation items
         donation_item1 = DonationItem(
-        description="Test Donation 1",
-        donation_date=datetime.today(),
-        amount=100.0,
-        donor_id=Donor.query.filter(Donor.email == 'john.doe2@example.com').first().donor_id,
-        charity_campaign_id=OrganizationCharityCampaign.query.filter(CharityCampaign.name == 'Charity campagin 1').first().id,
-        donation_type_id=DonationType.query.filter(DonationType.type == 'Food').first().id
+            description="Test Donation 1",
+            donation_date=datetime.today(),
+            amount=100.0,
+            donor_id=Donor.query.filter(Donor.email == 'john.doe2@example.com').first().donor_id,
+            charity_campaign_id=OrganizationCharityCampaign.query.filter(CharityCampaign.name == 'Charity campagin 1').first().id,
+            donation_type_id=DonationType.query.filter(DonationType.type == 'Food').first().id
         )
 
         donation_item2 = DonationItem(
@@ -286,19 +253,13 @@ def add_data():
         )
 
         # create charity campaign
-
- 
-
-        stock_1 = ItemStock( item_type_id=donation_item1.donation_type_id,
-                             campaign_id=donation_item1.charity_campaign_id,
-                             amount=donation_item1.amount)
+        stock_1 = ItemStock(item_type_id=donation_item1.donation_type_id,
+                            campaign_id=donation_item1.charity_campaign_id,
+                            amount=donation_item1.amount)
 
         stock_2 = ItemStock(item_type_id=donation_item2.donation_type_id,
-                             campaign_id=donation_item2.charity_campaign_id,
-                             amount=donation_item2.amount)
-
-
-
+                            campaign_id=donation_item2.charity_campaign_id,
+                            amount=donation_item2.amount)
         db.session.add(donation_item1)
         db.session.add(donation_item2)
         db.session.add(stock_1)
@@ -310,7 +271,7 @@ def add_data():
             status='PENDING',
             amount=10,
             req_address_id=Address.query.filter(Address.street == "456 Elm St" and Address.street_number == 'Apt 202' and Address.city == 'Los Angeles').first().id,
-            affected_id=Affected.query.filter(Affected.first_name=='John' and  Affected.last_name=='Affected').first().id,
+            affected_id=Affected.query.filter(Affected.first_name == 'John' and Affected.last_name == 'Affected').first().id,
             donation_type_id=DonationType.query.filter(DonationType.type == 'Food').first().id
 
         )
@@ -319,5 +280,3 @@ def add_data():
         db.session.commit()
 
         return redirect('/supply-chain')
-
-
