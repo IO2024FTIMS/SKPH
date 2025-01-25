b763502ca81c13aef48eb8a08c5462ccc1b5e04f
247751mo <162907800+247751mo@users.noreply.github.com>
2025-01-25 19:39:08 +0100

    Fix/affected (#40)

--- app/models/donation.py
+++ app/models/donation.py
@@ -1,37 +1,24 @@
 from datetime import date
-from enum import Enum
-from typing import TYPE_CHECKING
-
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.extensions import db
-
-if TYPE_CHECKING:
-    from app.models.donor import Donor
-
-
-class ItemDonationType(Enum):
-    WATER = "water"
-    FOOD = "food"
-    CLOTHES = "clothes"
-    MEDICINE = "medicine"
+from app.models.donor import Donor
 
 
 class DonationMoney(db.Model):
     donationMoney_id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str]
-    donation_date: Mapped[date] = mapped_column(default=date.today)
+    donation_date: Mapped[date] = date.today()
     donation_type: Mapped[str]
     cashAmount: Mapped[float]
-    charity_campaign = relationship('OrganizationCharityCampaign', back_populates='donations_money')
-    charity_campaign_id = mapped_column(ForeignKey('organization_charity_campaign.id'))
     donor_id: Mapped[int] = mapped_column(ForeignKey('donor.donor_id'))
-    donor: Mapped["Donor"] = relationship("Donor", back_populates="donations_money")
+    charity_campaign_id: Mapped[int] = mapped_column(ForeignKey('organization_charity_campaign.id'))
+    charity_campaign = relationship('OrganizationCharityCampaign')
+    donor: Mapped["Donor"] = relationship(back_populates="donations_money")
 
     def return_confirmation(self) -> str:
-        """Return a confirmation message."""
-        return f"Donation confirmed: {self.description}, Amount: {self.cashAmount}"
+        return f"Donation confirmed: id: {self.donationMoney_id}, {self.description}, Amount: {self.cashAmount}"
 
     def __repr__(self):
         return f"<Donation(description={self.description}, amount={self.cashAmount})>"
@@ -40,17 +27,21 @@ class DonationMoney(db.Model):
 class DonationItem(db.Model):
     donationItem_id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str]
-    donation_date: Mapped[date] = mapped_column(default=date.today)
-    donation_type: Mapped[ItemDonationType]
-    number: Mapped[int]
-    charity_campaign = relationship('OrganizationCharityCampaign', back_populates='donations_item')
-    charity_campaign_id = mapped_column(ForeignKey('organization_charity_campaign.id'))
+    donation_date: Mapped[date] = date.today()
+    donation_type_id: Mapped[int] = mapped_column(ForeignKey('donation_type.id'))
+    amount: Mapped[float]
     donor_id: Mapped[int] = mapped_column(ForeignKey('donor.donor_id'))
-    donor: Mapped["Donor"] = relationship("Donor", back_populates="donations_items")
+    donor: Mapped["Donor"] = relationship(back_populates="donations_items")
+    charity_campaign_id: Mapped[int] = mapped_column(ForeignKey('organization_charity_campaign.id'))
+    charity_campaign = relationship('OrganizationCharityCampaign')
 
     def return_confirmation(self) -> str:
-        """Return a confirmation message."""
-        return f"Donation confirmed: {self.description}, Amount: {self.number}"
+        return f"Donation confirmed: id: {self.donationItem_id}, {self.description}, Number: {self.amount}"
 
     def __repr__(self):
-        return f"<Donation(description={self.description}, amount={self.number})>"
+        return f"<Donation(description={self.description}, amount={self.amount})>"
+
+
+class DonationType(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str]
