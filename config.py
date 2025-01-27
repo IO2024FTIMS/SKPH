53d3615cfb27fec277dcab415e99688d493a2cce
unknown <wiktorszewczyk21@gmail.com>
2025-01-27 03:15:54 +0100

    Update config

--- config.py
+++ config.py
@@ -27,10 +27,7 @@ class DeploymentConfig(Config):
     DB_PORT = os.getenv('DB_PORT')
     DB_NAME = os.getenv('DB_NAME')
 
-    SQLALCHEMY_DATABASE_URI = (
-        f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}.postgres.database.azure.com:'
-        f'{DB_PORT}/{DB_NAME}?sslmode=require'
-    )
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}'
 
 
 config = {
