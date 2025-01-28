a66445201132eb73e75f6f7fa490b88da9220e75
Wiktor Szewczyk <76049470+wiktorszewczyk@users.noreply.github.com>
2025-01-28 01:00:02 +0100

    Live app deployment (#42)

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
@@ -12,14 +14,17 @@ from app.extensions import babel, db, get_locale, mail
 from app.maps.routes import bp as maps_bp
 from app.organization.routes import bp as organization_bp
 from app.reports.routes import bp as reports_bp
-from app.volunteers.routes import bp as volunteers_bp
 from app.supply_chain.routes import bp as supply_chain_bp
-from config import Config
+from app.volunteers.routes import bp as volunteers_bp
+
+from config import config
 
 
-def create_app(config_class=Config):
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_ENV', 'development')
     flask_app = Flask(__name__)
-    flask_app.config.from_object(config_class)
+    flask_app.config.from_object(config[config_name])
 
     # Initialize Flask extensions here
     db.init_app(flask_app)
@@ -34,19 +39,12 @@ def create_app(config_class=Config):
 
     # Register blueprints here
     flask_app.register_blueprint(auth_bp, url_prefix='/auth')
-
     flask_app.register_blueprint(reports_bp, url_prefix='/reports')
-
     flask_app.register_blueprint(volunteers_bp, url_prefix='/volunteers')
-
     flask_app.register_blueprint(chat_bp, url_prefix="/communication")
-
     flask_app.register_blueprint(affected_bp, url_prefix='/affected')
-
     flask_app.register_blueprint(donors_bp, url_prefix='/donors')
-
     flask_app.register_blueprint(organization_bp, url_prefix='/organizations')
-
     flask_app.register_blueprint(maps_bp, url_prefix='/maps')
 
     flask_app.register_blueprint(supply_chain_bp, url_prefix='/supply-chain')
--- config.py
+++ config.py
@@ -11,10 +11,26 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 
     # mail
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = os.getenv("MAIL_PORT")
-    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL") == 'True'
-    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == 'True'
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL') == 'True'
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') == 'True'
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+
+
+class DeploymentConfig(Config):
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOSTNAME = os.getenv('DB_HOSTNAME')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
+
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}'
+
+
+config = {
+    'development': Config,
+    'production': DeploymentConfig,
+}
